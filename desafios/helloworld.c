4f74ea8e099f45254138aa21537716ef30019c36
Gilmar Alberto <gilabreu00@gmail.com>
2023-12-15 14:33:15 -0300

    setlocale

--- desafios/helloworld.c
+++ desafios/helloworld.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	printf("Hello World!");
+	setlocale(LC_ALL, "Portuguese_Brasil");
+	printf("Hello World!\n");
+	printf("Olá Mundo!\n");
 	return 0;
 }
--- desafios/modelo.c
+++ desafios/modelo.c
@@ -2,8 +2,10 @@
 
 */
 #include <stdio.h>
+#include <locale.h>
 
 int main(){
+    setlocale(LC_ALL,"Portuguese_Brasil");
 
     return 0;
 }
