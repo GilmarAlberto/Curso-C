4978018cd125a6e1c0761b951106b01d646d41f8
Gilmar Alberto <gilabreu00@gmail.com>
2024-03-31 14:57:30 -0300

    desafios

--- desafios/desafio144.c
+++ desafios/desafio144.c
@@ -11,6 +11,24 @@ int main(){
     SetConsoleOutputCP(65001); //Permite a acentuação mas só funciona no windows. 
     int vetor[5];               
     
+    srand(time(NULL));
+
+    // Preenche o array com números aleatórios e imprime-os
+    printf("Array original:\n");
+
+    for(int i = 0; i < 5; i++){
+        vetor[i] = rand() % 101;
+        printf("%d ", vetor[i]);
+    }
+
+    // Verifica os valores pares e imprime o endereço das posições
+    printf("\nPosições contendo valores pares:\n");
+
+    for(int i = 0; i < 5; i++){
+        if(vetor[i] % 2 == 0){
+            printf("%d - %3d - %p\n", i, vetor[i], (void *)&vetor[i]);
+        }
+    }
 
     return 0;
 }
